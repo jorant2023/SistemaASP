251319c20ad1a900b99d6bf78b7eb0e8dca9c10b
jorant2023 <jorgeespejo@gmail.com>
2021-10-20 11:03:54 -0500

    Agregar archivos de proyecto.

---
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        public string nombre {get; set;}
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+
+    }
+}
