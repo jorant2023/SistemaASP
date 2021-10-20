d7713b25f56291918b74e8589e8a05dd9e58a6e4
jorant2023 <jorgeespejo@gmail.com>
2021-10-20 11:07:50 -0500

    comentario

--- Sistema.Entidades/Almacen/Categoria.cs
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -11,6 +11,7 @@ namespace Sistema.Entidades.Almacen
         public string nombre {get; set;}
         public string descripcion { get; set; }
         public bool condicion { get; set; }
+        //aaaaa
 
     }
 }
